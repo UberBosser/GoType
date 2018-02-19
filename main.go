0573b73409703112011b4891f4932d9165e843d0
Sergio <uberbosser@gmail.com>
2018-02-18 22:42:41 -0500

    Add multiplayer

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
     "net/http"
     "html/template"
     "io/ioutil"
-    "os"
+    "encoding/json"
+    "github.com/google/uuid"
+//    "time"
 )
 
 var text string
@@ -15,8 +17,46 @@ type webError struct {
     ErrorMessage string
 }
 
-type playText struct {
-    Text string
+type playTemplate struct {
+    Uuid    string
+    UserNumber int
+    Text    string
+}
+
+type userData struct {
+    Uuid    string `json:",omitempty"`
+    UserNumber int
+    Name    string
+    Wpm        int
+    Percentage int
+}
+
+type gameData struct {
+    Text        string
+    Time           int
+    ConnectedUsers int
+    Users   []userData
+}
+
+type gameLobby struct {
+    GameData gameData
+    Uuids    []string
+}
+
+func (g *gameLobby) create() {
+    t, _ := ioutil.ReadFile("texts.txt")
+    text = string(t)
+    log.Println(text)
+    g.GameData.Text = text
+}
+
+func (g *gameLobby) update() {
+}
+
+func (g *gameLobby) save() {
+    b, _ := json.Marshal(g.GameData)
+    // Save to file.
+    ioutil.WriteFile("./data/test.json", b, 0644)
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request, e *webError) { 
@@ -28,29 +68,51 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
     http.ServeFile(w, r, "./templates/index.html")
 }
 
-func playHandler(w http.ResponseWriter, r *http.Request) {
+func (g *gameLobby) playHandler(w http.ResponseWriter, r *http.Request) {
     if r.Method == "GET" {
-        t := template.Must(template.ParseFiles("./templates/play.tmpl"))
-        p := playText{Text: text}
-        t.Execute(w, p)
+        if (g.GameData.ConnectedUsers < 4) {
+            u, _ := uuid.NewRandom()
+            g.GameData.Users = append(g.GameData.Users, userData{UserNumber: g.GameData.ConnectedUsers, Name: "Guest"})
+            g.Uuids = append(g.Uuids, u.String())
+            te := playTemplate{Uuid: u.String(), UserNumber: g.GameData.ConnectedUsers, Text: g.GameData.Text} 
+            g.GameData.ConnectedUsers += 1
+            t := template.Must(template.ParseFiles("./templates/play.tmpl"))
+            t.Execute(w, te)
+        }
     } else {
         r.ParseForm()
         log.Println(r.Form["wpm"])
     }
 }
 
-func loginHandler(w http.ResponseWriter, r *http.Request) {}
+
+func (g *gameLobby) dataHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "GET" {
+        http.ServeFile(w, r, "./data/test.json") 
+    } else {
+        var u userData
+        decoder := json.NewDecoder(r.Body) 
+        decoder.Decode(&u)
+        if (u.Uuid == g.Uuids[int(u.UserNumber)]) {
+            u.Uuid = ""
+            if (g.GameData.Users[u.UserNumber].Percentage != 100) { 
+                g.GameData.Users[u.UserNumber].Wpm = u.Wpm
+                g.GameData.Users[u.UserNumber].Percentage = u.Percentage
+            }
+            g.save()
+        }
+    }
+}
 
 
 func main() {
-    file, _ := os.Open("texts.txt")
-    t, _ := ioutil.ReadAll(file)
-    text = string(t)
-    file.Close()
+    lobby := &gameLobby{}
+    lobby.create()
+    lobby.save()
 
     http.HandleFunc("/", homeHandler)
-    http.HandleFunc("/play", playHandler)
-    http.HandleFunc("/login", loginHandler)
+    http.HandleFunc("/play", lobby.playHandler)
+    http.HandleFunc("/data", lobby.dataHandler)
     
     fs := http.FileServer(http.Dir("static"))
     http.Handle("/static/", http.StripPrefix("/static/", fs))
--- static/playScript.js
+++ static/playScript.js
@@ -12,9 +12,13 @@ function getWord(text) {
     return word;
 }
 
+
 $(document).ready(function() {
     // Text to type, whole text.
     var typingText = $("#typingText");
+    typingText.text(typingText.text().replace("\n", ""));
+    // Use data like UUID and UserNumber. 
+    var userData = $("#userData");
     // Starting length of typingText.
     var typingTextSize = typingText.text().length;
     // Currently typing word.
@@ -22,24 +26,49 @@ $(document).ready(function() {
     // Already typed text.
     var typedText = $("#typedText");
     // Percentage & wpm bar.
-    var progressBar = $("#progressBar");
+    var progressBar = $("#userBar" + userData.attr("data-usernumber"));
     // Set starting word.
     currentWord.text(getWord(typingText));
     typingText.text(typingText.text().replace(currentWord.text(), ""));
     // To calculate wpm. 
     var startTime = new Date;
     // On input logic...
+    setInterval(function() {
+        // Percentage & wpm bar updating.
+        var percentage = (typedText.text().length/typingTextSize) * 100;
+        var wpm = Math.floor((typedText.text().length/5)/((new Date() - startTime)/60000));
+        // Send data.
+        $.ajax({
+            url: "/data",
+            type: "PUT",
+            data: JSON.stringify({
+                "Uuid": userData.attr("data-uuid"), 
+                "UserNumber": parseInt(userData.attr("data-usernumber")), 
+                "Wpm": wpm, 
+                "Percentage": Math.floor(percentage)
+            }),
+            contentType: "application/json" ,
+        });
+        var data;
+        // Get data & update Bars.
+        $.ajax({
+            url: "/data",
+            dataType: "json",
+            data: data,
+            success: function(data) {
+                for (var i = 0; i < data.Users.length; i++) {
+                    $("#userBar" + i).css("width", data.Users[i].Percentage + "%")
+                    $("#userBar" + i).text("wpm: " + data.Users[i].Wpm)
+                }
+            }
+        });
+    }, 1000);
     $("#inputBox").on('input', function() { 
         if ($(this).val() == currentWord.text()) {
             typedText.text(typedText.text() + currentWord.text());
             currentWord.text(getWord(typingText));
             typingText.text(typingText.text().replace(currentWord.text(), ""));
-            $(this).val("");
-            // Percentage & wpm bar updating.
-            var percentage = (typedText.text().length/typingTextSize) * 100;
-            var wpm = Math.floor((typedText.text().length/5)/((new Date() - startTime)/60000));
-            progressBar.css("width", percentage + "%");
-            progressBar.text("wpm: " + wpm);
+            $(this).val("");  
         } else if (currentWord.text().startsWith($(this).val())) {
             $(this).css("background-color", "#ccffcc");
         } else if ($(this).val() != "") {
